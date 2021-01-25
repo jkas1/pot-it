de67bd5356d9ec0e7cb8f437fc0b335d3a4c2f26
jkas1 <juliekasparian@gmail.com>
2021-01-24 23:33:05 -0800

    initial commit - moving project

---
+++ app.js
@@ -0,0 +1,52 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const app = express();
+const cors = require("cors");
+
+// import routes
+const productsRoute = require("./routes/products");
+const ordersRoute = require("./routes/orders");
+
+// CORS
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "PUT", "DELETE", "PATCH", "POST"],
+    allowedHeaders:
+      "Content-Type, Authorization, Origin, X-Requested-With, Accept",
+  })
+);
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api/products", productsRoute);
+app.use("/api/orders", ordersRoute);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
