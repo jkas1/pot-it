f15e95411854a2a7c015d93a0ba238384d5d23e8
jkas1 <juliekasparian@gmail.com>
2021-04-27 03:23:29 -0700

    replaced hero image and updated server url

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use("/api/products", productsRoute);
-// app.use("/api/orders", ordersRoute);
-app.use("/products", productsRoute);
-app.use("/orders", ordersRoute);
+app.use("/api/products", productsRoute);
+app.use("/api/orders", ordersRoute);
+// app.use("/products", productsRoute);
+// app.use("/orders", ordersRoute);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
